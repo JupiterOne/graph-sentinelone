8e2009f382f8b1840b0e7cfb9bdbeb212ff2bd22
Adam Williams <adam.williams@lifeomic.com>
2019-04-15 09:56:56 -0400

    Fix getClients() prematurely called

--- src/initializeContext.ts
+++ src/initializeContext.ts
@@ -1,11 +1,11 @@
 import {
   GraphClient,
   IntegrationExecutionContext,
-  IntegrationInstanceConfigError,
   IntegrationInvocationEvent,
   PersisterClient,
 } from "@jupiterone/jupiter-managed-integration-sdk";
-import { ProviderClient, ProviderConfig } from "./ProviderClient";
+
+import { ProviderClient } from "./ProviderClient";
 
 export interface SentinelOneExecutionContext
   extends IntegrationExecutionContext<IntegrationInvocationEvent> {
@@ -17,26 +17,9 @@ export interface SentinelOneExecutionContext
 export default function initializeContext(
   context: IntegrationExecutionContext<IntegrationInvocationEvent>,
 ): SentinelOneExecutionContext {
-  if (!context.instance.config) {
-    throw new Error(
-      "Provider config must be provided by the exectution environment",
-    );
-  }
-
-  if (!context.instance.config.apiToken || !context.instance.config.serverUrl) {
-    throw new IntegrationInstanceConfigError(
-      "Config sentinelOne apiToken, and serverUrl must be provided by the user",
-    );
-  }
-
-  const providerConfig: ProviderConfig = {
-    apiToken: context.instance.config.apiToken,
-    serverUrl: context.instance.config.serverUrl,
-  };
-
   return {
     ...context,
     ...context.clients.getClients(),
-    provider: new ProviderClient(providerConfig),
+    provider: new ProviderClient(context.instance.config),
   };
 }
