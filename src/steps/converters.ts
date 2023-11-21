54cfe5dc1b6a3ca4bf5defe88606bd3338a851b3
Gonzalo Avalos Ribas <gonzaloavalosribas@Gonzalos-MacBook-Pro.local>
2023-11-21 10:20:41 -0300

    Correction

--- src/steps/converters.ts
+++ src/steps/converters.ts
@@ -61,20 +61,22 @@ export function createGroupEntity(d: SentinelOneGroup): Entity {
 
 export function createAgentEntity(
   d: SentinelOneAgent,
-  logger: IntegrationLogger,
+  logger?: IntegrationLogger,
 ): Entity {
   const { licenseKey, ...agent } = d;
   const { tags, ...rawData } = agent;
   //temp - find the information about the tags an revert
   //they seem to be an object that we don't pase and its throwing upload errors
   //INT-9905
-  try {
-    logger.info(
-      { objectStructure: logObjectStructure(tags) },
-      'Object structure Log',
-    );
-  } catch (error) {
-    logger.info({ error }, 'There was an error while logging the structure');
+  if (logger) {
+    try {
+      logger.info(
+        { objectStructure: logObjectStructure(tags) },
+        'Object structure Log',
+      );
+    } catch (error) {
+      logger.info({ error }, 'There was an error while logging the structure');
+    }
   }
 
   return createIntegrationEntity({
